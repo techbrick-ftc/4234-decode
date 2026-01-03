3090687c398c89877fd4963368a55fbc471f2a70
aidenpantzer <aidenbpantzer@gmail.com>
2026-01-03 10:59:20 -0500

    Cleaned up code in SubFlywheel.java, and added PIDF coefficients for the flywheel motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/SubFlywheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/SubFlywheel.java
@@ -1,35 +1,39 @@
 package org.firstinspires.ftc.teamcode.SubSystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 public class SubFlywheel {
-    
+
     DcMotorEx flyWheel;
+
+    final double FLYWHEEL_MAX_RPM = 6000;
+    final double FLYWHEEL_MIN_RPM = 0;
+    final double FLYWHEEL_TICKS_PER_REVOLUTION = 21; // 6000 rpm motor
+
+    // For setVelocity tuning. TODO: Tune, test if working
+    final double FLYWHEEL_P = 2.5;
+    final double FLYWHEEL_I = 0.1;
+    final double FLYWHEEL_D = 0.2;
+    final double FLYWHEEL_F = 0.5;
 
     public SubFlywheel(HardwareMap hardwareMap) {
         flyWheel = hardwareMap.get(DcMotorEx.class, "flywheel1");
         flyWheel.setDirection(DcMotorSimple.Direction.REVERSE);
-        flyWheel.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        flyWheel.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT); // Allows coasting
+        PIDFCoefficients flywheelPIDF = new PIDFCoefficients(FLYWHEEL_P, FLYWHEEL_I, FLYWHEEL_D, FLYWHEEL_F);
+        flyWheel.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, flywheelPIDF);
+        flyWheel.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER); // TODO: DcMotor or DcMotorEX?
     }
 
-    public void setFlyWheelRPM(double targetFlywheelRPM) { //TODO: Test
-
-        // Set for 6000 RPM motor, may need to be changed?
-        final double ticksPerRevolution = 21;
-
-        // Gearing = output teeth / input teeth. Set, then change motor speed to directly set flywheel RPM.
-        double targetRPM = targetFlywheelRPM;
-
-        // Clamp targetRPM to (max: rpm of motor, min: 0)
-        if (targetRPM > 6000) {
-            targetRPM = 6000;
-        } else if (targetRPM < 0) {
-            targetRPM = 0;
-        }
-
-        flyWheel.setVelocity(targetRPM / 60 * ticksPerRevolution);
+    public void setFlyWheelRPM(double targetFlywheelRPM) {
+        flyWheel.setVelocity(
+                Math.max(FLYWHEEL_MIN_RPM,
+                Math.min(FLYWHEEL_MAX_RPM, targetFlywheelRPM))
+                * FLYWHEEL_TICKS_PER_REVOLUTION / 60.0 );
     }
 
 }
